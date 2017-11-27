6cd239599d0fe198d91c6c84a960c9a68c253ac5
Võ Lê Thanh Phú <volethanhphu174@gmail.com>
2017-11-27 05:34:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(count, holiday) {
-  return `It's ${days} days until ${holiday}!`
+  return `It's ${count} days until ${holiday}!`
 }
